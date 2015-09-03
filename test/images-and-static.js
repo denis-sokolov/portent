3a325bea47d7d9643452f4eb8b4f34f337cb4624
Denis Sokolov <denis@sokolov.cc>
2015-09-04 00:36:35 +0300

    Test: add tests to ensure images and statics respect underscore

--- test/images-and-static.js
+++ test/images-and-static.js
@@ -7,6 +7,12 @@ var test = function(name, imagePath, imageType, staticPath, staticType){
 	lib(name, staticPath, {type: staticType});
 };
 
+test.negative = function(name, imagePath, staticPath){
+	lib(name.replace('file', 'image'), imagePath, 404);
+	lib(name, staticPath, 404);
+};
+
+
 test('includes a simple file',
 	'/img/siluette-tree.jpg', 'image/jpeg',
 	'/static/hi.txt', 'text/plain');
@@ -15,6 +21,9 @@ test('includes a file deeper in the tree',
 	'/img/deeper/pamukkale-terraces.jpg', 'image/jpeg',
 	'/static/deeper/sample.mp4', 'video/mp4');
 
-test('includes an image with a space',
+test('includes an file with a space',
 	'/img/circle maze.jpg', 'image/jpeg',
 	'/static/with a space.txt', 'text/plain');
+
+test.negative('respect _ in the file name',
+	'/img/_red-phone-box.jpg', '/static/_my-internal-notes.txt');
