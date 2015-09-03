53fec205d29de3e2fc65fa777517f4832201ca77
Denis Sokolov <denis@sokolov.cc>
2015-09-03 15:11:59 +0300

    Test image deeper in the hierarchy

--- test/images.js
+++ test/images.js
@@ -5,3 +5,6 @@ var test = require('./lib');
 test('includes a simple image', '/img/siluette-tree.jpg', {
 	type: 'image/jpeg'
 });
+test('includes an image deeper in the tree', '/img/deeper/pamukkale-terraces.jpg', {
+	type: 'image/jpeg'
+});
