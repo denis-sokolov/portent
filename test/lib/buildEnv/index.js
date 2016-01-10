9382c0985ed63b43a97cb03b5f3e3ce256a017ed
Denis Sokolov <denis@sokolov.cc>
2016-01-10 21:01:40 +0200

    Improved test/lib filename resolution in build tests

--- test/lib/buildEnv/index.js
+++ test/lib/buildEnv/index.js
@@ -13,20 +13,22 @@ module.exports = function(fixtureDir){
 		var request = function(requestPath) {
 			return new Promise(function(resolve){
 				var filepath = diskEnv.dest + '/' + requestPath +
-				(requestPath.substring(requestPath.length - 1) === '/' ? 'index' : '');
+				(requestPath.substring(requestPath.length - 1) === '/' ? 'index' : '') +
+
+				// This is a cheap way to distinguish whether we need .html
+				// Semantically this information belongs in individual tests,
+				// but our naming pattern is clear and this allows for simpler test cases.
+				// This also has no effect on production.
+				(requestPath.match(/[^\/]\.[a-z0-9]+$/) ? '' : '.html');
 
 				var contents;
 				try {
 					contents = fs.readFileSync(filepath, 'utf-8');
 				} catch (err) {
-					try {
-						contents = fs.readFileSync(filepath + '.html', 'utf-8');
-					} catch (err2) {
-						return resolve({
-							text: '',
-							code: 404
-						});
-					}
+					return resolve({
+						text: '',
+						code: 404
+					});
 				}
 
 				resolve({
