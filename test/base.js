5c76eb9c7ec4bfe8bfdac82183d2228814851f0b
Denis Sokolov <denis@sokolov.cc>
2016-02-21 13:12:53 +0200

    Fix base tests

--- test/base.js
+++ test/base.js
@@ -4,22 +4,22 @@ var test = require('./lib');
 
 test('adds <base> in development', '/baseless', {
 	build: false,
-	contains: '<base href="http://127.0.0.1:'
+	contains: '<base href=http://127.0.0.1:'
 });
 
 test('rewrites <base> in development', '/', {
 	build: false,
-	contains: '<base href="http://127.0.0.1:'
+	contains: '<base href=http://127.0.0.1:'
 });
 
 test('does not leave old <base> in development', '/', {
 	build: false,
-	doesNotContain: '<base href="http://example.com/'
+	doesNotContain: '<base href=http://example.com/'
 });
 
 test('keeps <base> in production', '/', {
 	server: false,
-	contains: '<base href="http://example.com/'
+	contains: '<base href=http://example.com/'
 });
 
 test.buildWarning('warns about a missing <base>', 'Missing <base>.');
